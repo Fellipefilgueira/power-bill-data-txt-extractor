70c17b78971332fffe5e35ce429253a960ddc6b7
Fellipefilgueira <fellipellucas@gmail.com>
2022-03-23 18:40:05 -0300

    Data extract ajust

--- Source/compute_B.py
+++ Source/compute_B.py
@@ -30,7 +30,7 @@ def compute_B(j, text, data):
             data['Consumo reativo excedente (kvarh)'] = float(line[75:87])/100
             data['Despesa consumo reativo excedente (R$)'] = float(line[100:113])/100 
         if line.find("14") == 0 and line.find("ZDIFB") == 69: # vai apresentar problema quando um mês possuir duas bandeiras
-            bandeira = line[ 133 : line.find('\n') ]
+            bandeira = line[ 132 : line.find('\n') ]
             if bandeira == 'VERDE':
                 data['VERDE Consumo ativo (kWh)'] = CAT 
                 data['VERDE Despesa consumo ativo (R$)'] = XCAT + YCAT
