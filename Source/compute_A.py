820fb859349c66dcc895071c7da9423ee2cb2cf7
Fellipefilgueira <fellipellucas@gmail.com>
2022-05-24 17:50:36 -0300

    Otimized code to 2019 and before bills

--- Source/compute_A.py
+++ Source/compute_A.py
@@ -2,11 +2,30 @@ def compute_A(j, text, data):
 
     from datetime import datetime
 
-    # set variables 
+    # set lists 
+
+    DEMNP_lst = []
+    DEMFP_lst = []
+    CRNP_lst = []
+    CRFP_lst = []
+    CRENP_lst = []
+    CREFP_lst =[]
+    DMCRNP_lst = []
+    DMCRFP_lst = []
+
+    # set variables
 
     ciclo = 0
+    ciclo_bd1 = 0
+    ciclo_bd2 = 0
     CANP = 0
+    CANPD = 0
+    CANPA = 0
+    CANPM = 0
     CAFP = 0
+    CAFPD = 0
+    CAFPA = 0
+    CAFPM = 0
     XCANP = 0
     XCAFP = 0
     YCANP = 0
@@ -14,7 +33,10 @@ def compute_A(j, text, data):
     DEMNP = 0
     DEMFP = 0
     
-    for line in text[j]:
+    for line in text[j]: 
+
+        # code for bills in 2020 or after
+
         if line.find('11') == 0:
             data['Conta contrato'] = line[26:36]
         if line.find('12') == 0 and line.find('A4') == 81:
@@ -22,34 +44,44 @@ def compute_A(j, text, data):
             data['Data de leitura'] = datetime.strptime(f'{line[102:110]}', '%d%m%Y').strftime('%d/%m/%Y')
             data['Valor da fatura (R$)'] = float(line[138:151])/100
             data['Demanda ativa contratada (kW)'] = float(line[187:216])/100
+            d0 = datetime.strptime(f'{line[118:126]}', '%d%m%Y') 
+            d1 = datetime.strptime(f'{line[126:134]}', '%d%m%Y')
+            ciclo = int(abs((d1-d0).days))+1
         if line.find('12') == 0 and line.find('A3') == 81:
             data['Classificação'] = line[81:83]
             data['Data de leitura'] = datetime.strptime(f'{line[102:110]}', '%d%m%Y').strftime('%d/%m/%Y')
             data['Valor da fatura (R$)'] = float(line[138:151])/100
             data['Demanda ativa contratada NP (kW)'] = float(line[187:216])/100
             data['Demanda ativa contratada FP (kW)'] = float(line[216:228])/100
-        if line.find('13') == 0 and line.find('CANP') == 87:
-            d0 = datetime.strptime(f'{line[93:101]}', '%d%m%Y') 
-            d1 = datetime.strptime(f'{line[101:109]}', '%d%m%Y')
-            ciclo = int(abs((d1-d0).days))+1
+            data['Demanda ativa contratada (kW)'] = float(line[187:216])/100
+            d0 = datetime.strptime(f'{line[118:126]}', '%d%m%Y') 
+            d1 = datetime.strptime(f'{line[126:134]}', '%d%m%Y')
         if line.find('13') == 0 and line.find('DEMNP') == 87:
-            DEMNP = float(line[151:166])/100
+            DEMNP_lst.append(float(line[151:166])/100)
+            DEMNP = sum(DEMNP_lst)
             data['Demanda máxima NP (kW)'] = DEMNP
         if line.find('13') == 0 and line.find('DEMFP') == 87:
-            DEMFP = float(line[151:166])/100
+            DEMFP_lst.append(float(line[151:166])/100)
+            DEMFP = sum(DEMFP_lst)
             data['Demanda máxima FP (kW)'] = DEMFP
         if line.find('13') == 0 and line.find('CRNP') == 87:
-            data['Consumo reativo NP (kvarh)'] = float(line[151:166])/100
+            CRNP_lst.append(float(line[151:166])/100)
+            data['Consumo reativo NP (kvarh)'] = sum(CRNP_lst)
         if line.find('13') == 0 and line.find('CRFP') == 87:
-            data['Consumo reativo FP (kvarh)'] = float(line[151:166])/100
+            CRFP_lst.append(float(line[151:166])/100)
+            data['Consumo reativo FP (kvarh)'] = sum(CRFP_lst)
         if line.find('13') == 0 and line.find('CRENP') == 87:
-            data['Consumo reativo excedente NP (kvarh)'] = float(line[151:166])/100     
+            CRENP_lst.append(float(line[151:166])/100)
+            data['Consumo reativo excedente NP (kvarh)'] = sum(CRENP_lst)
         if line.find('13') == 0 and line.find('CREFP') == 87:
-            data['Consumo reativo excedente FP (kvarh)'] = float(line[151:166])/100  
-        if line.find('13') == 0 and line.find('DMCRNP') == 87: 
-            data['Demanda máxima corrigida NP (kW)'] = float(line[151:166])/100
+            CREFP_lst.append(float(line[151:166])/100)
+            data['Consumo reativo excedente FP (kvarh)'] = sum(CREFP_lst)
+        if line.find('13') == 0 and line.find('DMCRNP') == 87:
+            DMCRNP_lst.append(float(line[151:166])/100)
+            data['Demanda máxima corrigida NP (kW)'] = sum(DMCRNP_lst)
         if line.find('13') == 0 and line.find('DMCRFP') == 87:
-            data['Demanda máxima corrigida FP (kW)'] = float(line[151:166])/100              
+            DMCRFP_lst.append(float(line[151:166])/100)
+            data['Demanda máxima corrigida FP (kW)'] = sum(DMCRFP_lst)
         if line.find('14') == 0 and line.find('ZDEM') == 69:
             data['Demanda ativa (kW)'] = float(line[75:87])/100
             data['Despesa demanda ativa (R$)'] = float(line[100:113])/100
@@ -111,36 +143,51 @@ def compute_A(j, text, data):
             data['Despesa consumo reativo excedente FP (R$)'] = float(line[100:113])/100
         if line.find('14') == 0 and line.find('WHTAX ') == 69:
             data['Tributo (R$)'] = float(line[99:113])/100
-        
+
+        #
         # code for bills in 2019 or before 
+        #
+        #
+        #
 
         if line.find('14') == 0 and line.find('ZCANP') == 69:
-            CANP = float(line[75:87])/100
             ZCANP = float(line[100:113])/100 
-            data['Consumo ativo NP (kWh)'] = ZCANP
         if line.find('14') == 0 and line.find('ZCAFP') == 69:
-            CAFP = float(line[75:87])/100
             ZCAFP = float(line[100:113])/100 
-            data['Consumo ativo FP (kWh)'] = ZCAFP
-            data['Fator de carga NP'] = round( CANP / ( DEMNP * ( ciclo - 8 )*3 ), 2)
-            data['Fator de carga FP'] = round( CAFP / ( DEMFP * ( ( ciclo - 8 )*21 + 8*24 )), 2)
-        if line.find('ZCAFPD') == 69: # check code with green flag in old files 
-            data['VERDE Consumo ativo NP (kWh)'] = CANP
-            data['VERDE Consumo ativo FP (kWh)'] = CAFP                   
+        if line.find('ZCANPD') == 69: # check code with green flag in old files 
+            CANPD = float(line[75:87])/100
+            data['VERDE Consumo ativo NP (kWh)'] = CANPD
             data['VERDE Despesa consumo ativo NP (R$)'] = ZCANP
+        if line.find('ZCANPA') == 69:
+            CANPA = float(line[75:87])/100
+            data['AMARELA Consumo ativo NP (kWh)'] = CANPA
+            data['AMARELA Despesa consumo ativo NP (R$)'] = ZCANP
+        if line.find('ZCANPM') == 69: # check code with red flag in old files 
+            CANPM = float(line[75:87])/100
+            data['VERMELHA Consumo ativo NP (kWh)'] = CANPM
+            data['VERMELHA Despesa consumo ativo NP (R$)'] = ZCANP
+        if line.find('ZCAFPD') == 69: # check code with green flag in old files 
+            CAFPD = float(line[75:87])/100
+            data['VERDE Consumo ativo FP (kWh)'] = CAFPD                   
             data['VERDE Despesa consumo ativo FP (R$)'] = ZCAFP
             data['Dias bandeira VERDE'] = ciclo                       
         if line.find('ZCAFPA') == 69:
-            data['AMARELA Consumo ativo NP (kWh)'] = CANP
-            data['AMARELA Consumo ativo FP (kWh)'] = CAFP                   
-            data['AMARELA Despesa consumo ativo NP (R$)'] = ZCANP
+            CAFPA = float(line[75:87])/100
+            data['AMARELA Consumo ativo FP (kWh)'] = CAFPA                  
             data['AMARELA Despesa consumo ativo FP (R$)'] = ZCAFP
             data['Dias bandeira AMARELA'] = ciclo
         if line.find('ZCAFPM') == 69: # check code with red flag in old files  
-            data['VERMELHA Consumo ativo NP (kWh)'] = CANP
-            data['VERMELHA Consumo ativo FP (kWh)'] = CAFP                   
-            data['VERMELHA Despesa consumo ativo NP (R$)'] = ZCANP
+            CAFPM = float(line[75:87])/100
+            data['VERMELHA Consumo ativo FP (kWh)'] = CAFPM                  
             data['VERMELHA Despesa consumo ativo FP (R$)'] = ZCAFP
-            data['Dias bandeira VERMELHA'] = ciclo  
+            data['Dias bandeira VERMELHA'] = ciclo 
+        if line.find('14') == 0 and line.find('ZCANP') == 69:
+            CANP = CANPD + CANPA + CANPM
+            data['Consumo ativo NP (kWh)'] = CANP
+            data['Fator de carga NP'] = round( CANP / ( DEMNP * ( ciclo - 8 )*3 ), 2)
+        if line.find('14') == 0 and line.find('ZCAFP') == 69:
+            CAFP = CAFPD + CAFPA + CAFPM
+            data['Consumo ativo FP (kWh)'] = CAFP
+            data['Fator de carga FP'] = round( CAFP / ( DEMFP * ( ( ciclo - 8 )*21 + 8*24 )), 2)
 
     return(data)
--- Source/compute_B.py
+++ Source/compute_B.py
@@ -2,11 +2,28 @@ def compute_B(j, text, data):
     
     from datetime import datetime
 
+    # set lists 
+
+    ZDIFB_lst = []
+
     # set variables 
 
     ciclo = 0
+    CAT = 0
+    XCAT = 0
+    YCAT = 0
+    ZCAT = 0
+    ZDIFB = 0
 
     for line in text[j]:
+
+        # code for bills in 2019 or before 
+        
+        if line.find("14") == 0 and line.find("ZCAT") == 69:
+            CAT = float(line[75:87])/100
+            data['Consumo ativo (kWh)'] = CAT
+            ZCAT = float(line[100:113])/100
+
         if line.find("11") == 0:
             data['Conta contrato'] = line[26:36]
         if line.find("12") == 0:
@@ -28,40 +45,59 @@ def compute_B(j, text, data):
             data['Despesa consumo ativo (R$)'] = (XCAT + YCAT)
         if line.find("14") == 0 and line.find("ZCRE") == 69:
             data['Consumo reativo excedente (kvarh)'] = float(line[75:87])/100
-            data['Despesa consumo reativo excedente (R$)'] = float(line[100:113])/100 
-        if line.find("YCATD") == 69: # vai apresentar problema quando um mês possuir duas bandeiras
-            data['VERDE Consumo ativo (kWh)'] = CAT 
-            data['VERDE Despesa consumo ativo (R$)'] = (XCAT + YCAT)
-            data['Dias bandeira VERDE'] = ciclo
-        if line.find("YCATA") == 69:
+            data['Despesa consumo reativo excedente (R$)'] = float(line[100:113])/100
+        if line.find("ZDIFBA") == 69:
             data['AMARELA Consumo ativo (kWh)'] = CAT
-            data['AMARELA Despesa consumo ativo (R$)'] = (XCAT + YCAT)
+            data['AMARELA Despesa consumo ativo (R$)'] = XCAT + YCAT + ZCAT
             data['Dias bandeira AMARELA'] = ciclo
-        if line.find("YCATM") == 69:
+        if line.find("ZDIFBM") == 69:
             data['VERMELHA Consumo ativo (kWh)'] = CAT 
-            data['VERMELHA Despesa consumo ativo (R$)'] =  (XCAT + YCAT)
+            data['VERMELHA Despesa consumo ativo (R$)'] =  XCAT + YCAT + ZCAT
             data['Dias bandeira VERMELHA'] = ciclo
+        if line.find("ZDIFB") == 69:
+            ZDIFB_lst.append(1)
+            ZDIFB = sum(ZDIFB_lst)
+            if ZDIFB == 0:
+                data['VERDE Consumo ativo (kWh)'] = CAT 
+                data['VERDE Despesa consumo ativo (R$)'] = XCAT + YCAT + ZCAT
+                data['Dias bandeira VERDE'] = ciclo
+        else: 
+            ZDIFB_lst.append(0)
+            ZDIFB = sum(ZDIFB_lst)
+            if ZDIFB == 0:
+                data['VERDE Consumo ativo (kWh)'] = CAT 
+                data['VERDE Despesa consumo ativo (R$)'] = XCAT + YCAT + ZCAT
+                data['Dias bandeira VERDE'] = ciclo
         if line.find('14') == 0 and line.find('WHTAX') == 69:
             data['Tributo (R$)'] = float(line[99:113])/100
 
-        # code for bills in 2019 or before 
+        # ZDIFB_lst.append(line.find("ZDIFB"))
+        # ZDIFB_avg = sum(ZDIFB_lst)/len(ZDIFB_lst)
+        # if not 69 in ZDIFB_lst:
+        #     data['VERDE Consumo ativo (kWh)'] = CAT 
+        #     data['VERDE Despesa consumo ativo (R$)'] = XCAT + YCAT + ZCAT
+        #     data['Dias bandeira VERDE'] = ciclo
+
+        # # code for bills in 2019 or before 
         
-        if line.find("14") == 0 and line.find("ZCAT") == 69:
-            CAT = float(line[75:87])/100
-            data['Consumo ativo (kWh)'] = CAT
-            ZCAT = float(line[100:113])/100
-        if line.find('ZCATD') == 69:
-            data['VERDE Consumo ativo (kWh)'] = CAT 
-            data['VERDE Despesa consumo ativo (R$)'] = ZCAT
-            data['Dias bandeira VERDE'] = ciclo
-        if line.find('ZCATA') == 69:
-            data['AMARELA Consumo ativo (kWh)'] = CAT
-            data['AMARELA Despesa consumo ativo (R$)'] = ZCAT
-            data['Dias bandeira AMARELA'] = ciclo
-        if line.find('ZCATM') == 69:
-            data['VERMELHA Consumo ativo (kWh)'] = CAT 
-            data['VERMELHA Despesa consumo ativo (R$)'] =  ZCAT
-            data['Dias bandeira VERMELHA'] = ciclo
+        # if line.find("14") == 0 and line.find("ZCAT") == 69:
+        #     CAT = float(line[75:87])/100
+        #     data['Consumo ativo (kWh)'] = CAT
+        #     ZCAT = float(line[100:113])/100
+        # if line.find("ZDIFB") == 69:
+        #     ZDIFB_count = ZDIFB_count + 1
+        # if ZDIFB_count == 0:
+        #     data['VERDE Consumo ativo (kWh)'] = CAT 
+        #     data['VERDE Despesa consumo ativo (R$)'] = ZCAT
+        #     data['Dias bandeira VERDE'] = ciclo
+        # if line.find('ZDIFBA') == 69:
+        #     data['AMARELA Consumo ativo (kWh)'] = CAT
+        #     data['AMARELA Despesa consumo ativo (R$)'] = ZCAT
+        #     data['Dias bandeira AMARELA'] = ciclo
+        # if line.find('ZDIFBM') == 69:
+        #     data['VERMELHA Consumo ativo (kWh)'] = CAT 
+        #     data['VERMELHA Despesa consumo ativo (R$)'] =  ZCAT
+        #     data['Dias bandeira VERMELHA'] = ciclo
         
 
     return(data)
